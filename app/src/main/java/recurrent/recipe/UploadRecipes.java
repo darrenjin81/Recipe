01e80f5f5b909339d5930d70282ee17116059503
IhuanGunawan <ihuan.gunawan@gmail.com>
2017-09-25 14:28:42 +1000

    upload validation

--- app/src/main/java/recurrent/recipe/UploadRecipes.java
+++ app/src/main/java/recurrent/recipe/UploadRecipes.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -24,37 +25,40 @@ public class UploadRecipes extends Fragment {
     // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
-        // Setup any handles to view objects here
-        // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+
         final EditText nameEditor = (EditText) view.findViewById(R.id.edit_recipe_name);
         final EditText instructionsEditor = (EditText) view.findViewById(R.id.edit_recipe_instructions);
 
         view.findViewById(R.id.button_upload).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO i should make sure its not empty
-                //firebase stuff
+
                 FirebaseDatabase database = FirebaseDatabase.getInstance();
                 DatabaseReference myRef = database.getReference();
 
                 String name = nameEditor.getText().toString();
                 String instructions = instructionsEditor.getText().toString();
 
+                String emptyTitleError = "Please enter a title.";
+                if(name.isEmpty()){
+                    Toast t = Toast.makeText(getContext(), emptyTitleError, Toast.LENGTH_SHORT);
+                    t.show();
+                }
 
-                //write into database
-                //writeNewUser(myRef, name, instructions);
-                Recipe recipe = new Recipe(name, instructions);
-                myRef.child("recipes").child(name).setValue(recipe);
+                if(!name.isEmpty()){
+                    //write into database
+                    Recipe recipe = new Recipe(name, instructions);
+                    myRef.child("recipes").child(name).setValue(recipe);
 
-                Bundle args = new Bundle();
-                args.putParcelable(RecipeView.RecipeArgKey, recipe);
-                Fragment nextFrag= new RecipeView();
-                nextFrag.setArguments(args);
-                getActivity().getSupportFragmentManager().beginTransaction()
-                        .replace(((ViewGroup)getView().getParent()).getId(), nextFrag, "r")
-                        .addToBackStack(null)
-                        .commit();
-
+                    Bundle args = new Bundle();
+                    args.putParcelable(RecipeView.RecipeArgKey, recipe);
+                    Fragment nextFrag= new RecipeView();
+                    nextFrag.setArguments(args);
+                    getActivity().getSupportFragmentManager().beginTransaction()
+                            .replace(((ViewGroup)getView().getParent()).getId(), nextFrag, "r")
+                            .addToBackStack(null)
+                            .commit();
+                }
             }
         });
     }
