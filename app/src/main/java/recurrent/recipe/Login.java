9c56dc77f9a2647826bc54afbec03d6402e09a83
darrenjin81 <jindarren81@gmail.com>
2017-09-25 13:50:10 +1000

    Authentication done.

--- app/src/main/java/recurrent/recipe/Login.java
+++ app/src/main/java/recurrent/recipe/Login.java
@@ -16,12 +16,13 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class Login extends Fragment {
     private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
+    //FirebaseAuth.AuthStateListener mAuthListener;
     private DatabaseReference mRef;
     private ProgressDialog mProgress;
 
@@ -32,26 +33,21 @@ public class Login extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_login, parent, false);
-    }
-
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        // Setup any handles to view objects here
+        View view = inflater.inflate(R.layout.fragment_login, parent, false);
         mAuth = FirebaseAuth.getInstance();
         mProgress = new ProgressDialog(getActivity());
         mRef = FirebaseDatabase.getInstance().getReference().child("users");
 
-        //Check is user has logged in or not
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                if (firebaseAuth.getCurrentUser() != null) {
-                    //TODO ....IDK HOW TO FIREBASE
+//        //Check is user has logged in or not
+//        mAuthListener = new FirebaseAuth.AuthStateListener() {
+//            @Override
+//            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+//                if (firebaseAuth.getCurrentUser() != null) {
+//                    //TODO ....IDK HOW TO FIREBASE
 //                    startActivity(new Intent(getActivity(), UserProfile.class));
-                }
-            }
-        };
+//                }
+//            }
+//        };
 
         etEmail = (EditText) view.findViewById(R.id.etLoginEmailField);
         etPassword = (EditText) view.findViewById(R.id.etLoginPasswordField);
@@ -60,46 +56,63 @@ public class Login extends Fragment {
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Fragment fragment = new Register();
-                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-                fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+                createAccount(etEmail.getText().toString(), etPassword.getText().toString());
             }
         });
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 signIn(etEmail.getText().toString(), etPassword.getText().toString());
-                //TODO HOW TO FIREBASE????
             }
         });
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+
 
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
+        if (mAuth.getCurrentUser() != null){
+            Fragment fragment = new UserProfile(mAuth.getCurrentUser().getUid());
+            FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+        }
     }
 
     private void signIn(String email, String password) {
-        //TODO: check if email and password are valid or not.
         if (email.isEmpty() || password.isEmpty()) {
             Toast.makeText(getActivity(), "empty email or empty password", Toast.LENGTH_LONG).show();
         } else {
             mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    if (task.isSuccessful()) {
-                        //Toast.makeText(getActivity(), "sign in problem", Toast.LENGTH_LONG).show();
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            //Toast.makeText(getActivity(), "sign in problem", Toast.LENGTH_LONG).show();
+                            String user_id = mAuth.getCurrentUser().getUid();
+                            FirebaseUser usr = mAuth.getCurrentUser();
+                            DatabaseReference current_user = mRef.child(user_id);
+                            current_user.child("name").setValue(usr.getEmail());
+                            current_user.child("image").setValue("default");
+
+                            Fragment fragment = new UserProfile(mAuth.getCurrentUser().getUid());
+                            FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                            fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+                        }else{
+                            Toast.makeText(getActivity(), "sign in problem", Toast.LENGTH_LONG).show();
+                        }
                     }
-                }
-            });
-        }
+                });
+            }
     }
 
     private void createAccount(final String email, String password) {
         if (email.isEmpty() || password.isEmpty()) {
-            //Toast.makeText(getActivity(), "empty email or empty password", Toast.LENGTH_LONG).show();
+            Toast.makeText(getActivity(), "empty email or empty password", Toast.LENGTH_LONG).show();
         } else {
 
             mProgress.setMessage("signing up");
@@ -115,6 +128,11 @@ public class Login extends Fragment {
                         current_user.child("image").setValue("default");
 
                         mProgress.dismiss();
+
+                        Fragment fragment = new UserProfile(mAuth.getCurrentUser().getUid());
+                        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
                     }
                 }
             });
